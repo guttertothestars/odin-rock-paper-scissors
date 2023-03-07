b5e6463a40418f7a604fe0da5937a5db81e8b138
guttertothestars <wwilkson@gmail.com>
2023-03-07 13:04:06 -0500

    add getPlayerChoice function, w looping validation

--- rps.js
+++ rps.js
@@ -1,6 +1,23 @@
+/* global variables */
+let playerScore = 0;
+let computerScore = 0;
+let roundCount =0;
+
+/* the three play options */
 const choices = ['rock', 'paper', 'scissors'];
 
-function getComputerChoice(); {
-  let choice = choices[Math.floor(Math.random() * choices.length)];
-  return choice;
+/* randomly returns one of the three play options */
+function getComputerChoice() {
+  let coputerChoice = CHOICES[Math.floor(Math.random() * CHOICES.length)];
+  return computerChoice;
 } 
+
+/* solicits player choice of the options, w/validation */
+function getPlayerChoice() {
+  let playerChoice = "";
+  while (!choices.includes(playerChoice)) {
+    playerChoice = prompt("Choose: Rock, Paper, or Scissors").toLocaleLowerCase();
+  }
+  return playerChoice;
+}
+
